d9c16629e77e3557d6b37cff3f8d7fbd4d3d5a68
Geunwon Park <rmsdnjsaos@gmail.com>
2026-01-30 18:24:26 +0900

    #1 chore(project): 프로젝트 폴더 구조 & CSS 설정
    - Tailwind CSS 커스텀 테마 설정
    - 컴포넌트 폴더 구조 구성 (common, layout, todo)
    - 커스텀 훅 파일 생성 (useTodos, useTodoDetail)
    - lib 폴더 구성 (api, type, utils)
    - public/assets 폴더 및 아이콘/이미지 리소스 추가
    
    Resolves: #1
    See also: None

---
+++ tailwind.config.ts
@@ -0,0 +1,48 @@
+import type {Config} from "tailwindcss";
+
+const config: Config = {
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extends: {
+            colors: {
+                slate: {
+                    900: '#0F172A',
+                    800: '#1E293B',
+                    500: '#64748B',
+                    400: '#94A3B8',
+                    300: '#CBD5E1',
+                    200: '#E2E8F0',
+                    100: '#F1F5F9',
+                },
+
+                violet: {
+                    600: '#7C3AED',
+                    100: '#EDE9FE',
+                },
+
+                rose: {
+                    500: '#F43F5E',
+                },
+                lime: {
+                    300: '#BEF264',
+                },
+                amber: {
+                    800: '#92400E',
+                },
+            },
+
+            screens: {
+                'mobile': '375px',
+                'tablet': '844px',
+                'desktop': '1920px',
+            },
+        },
+    },
+    plugins: [],
+}
+
+export default config;
